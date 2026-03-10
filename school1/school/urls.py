38dd1522cee1a367c56ff5baadb1def7bda10f5d
veronika-create <veranikayalanskaya@yandex.ru>
2026-03-10 12:02:49 +0300

    Создание первой базы данных для приложения lessons

--- school1/school/urls.py
+++ school1/school/urls.py
@@ -1,6 +1,8 @@
+from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
     path('std/', include('lessons.urls', namespace='lessons')),
     path('adm/', include('users.urls', namespace='users')),
