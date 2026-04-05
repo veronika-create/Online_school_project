b915ae2a68a950c20f4776e97c3104bfd396a0b0
veronika-create <veranikayalanskaya@yandex.ru>
2026-04-05 16:12:18 +0300

    Настройка формы валидации для регистрации

--- school1/school/settings.py
+++ school1/school/settings.py
@@ -98,6 +98,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'OPTIONS': {
+            'timeout': 30,  # в секундах
+        }
     }
 }
 
